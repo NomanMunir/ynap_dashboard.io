6b6a53a2ff5e718d64326b588af705fc34dc0669
NomanMunir <mrnomanmunir@gmail.com>
2022-07-12 05:36:46 +0400

    New Feature add: now we can add or drag multiple files at the same time.

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const parseData = (file) => {
+const parseData = (files) => {
     spinner.innerHTML = spinnerElem;
-    Papa.parse(file, {
-        header: true,
-        skipEmptyLines: true,
-        dynamicTyping: true,
-        complete: function (results) {
-            CONFIG.parsedData = results.data;
-            segregateByName()
-            spinner.innerHTML = ""
-        }
+    // const file = files[0]
+
+    Object.values(files).forEach(file => {
+        Papa.parse(file, {
+            header: true,
+            skipEmptyLines: true,
+            dynamicTyping: true,
+            complete: function (results) {
+                CONFIG.parsedData.push(...results.data)
+                segregateByName()
+
+            }
+        })
     });
 }
 // Handling input files Here 
@@ -16,12 +20,11 @@ function fileChangeHandler(e) {
     e.preventDefault();
     const fileElem = document.querySelector('#fileElem');
     console.log(fileElem.files)
-    const file = fileElem.files[0]
-    if (!file.name.endsWith('csv')) {
+    if (!fileElem.files[0].name.endsWith('csv')) {
         alert("Please select csv file")
         return
     }
-    parseData(file)
+    parseData(fileElem.files)
 }
 
 document.querySelector('#fileElem').addEventListener('change', fileChangeHandler)
